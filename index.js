74d9e9f9347624ef45f0b10ed3b1d7a2785d00f6
Drew Keat <thedrewkeat@gmail.com>
2021-09-19 14:52:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email) {
+  // Why is the 'return' command necessary for the catch method?
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((resp) => resp.json())
+    .then((object) => (document.body.innerHTML = object.id))
+    .catch((error) => (document.body.innerHTML = error.message));
+}
